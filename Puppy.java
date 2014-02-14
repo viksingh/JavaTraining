a0b7d29f7011ce43b607de50c8623db44af0f792
viksingh <vikas.singh.0@gmail.com>
2014-02-14 13:59:26 +1100

    Create Puppy.java

---
+++ Puppy.java
@@ -0,0 +1,12 @@
+package demo.training;
+
+public class Puppy {
+	  public Puppy(String name) {
+	    // This constructor has one parameter, name.
+	    System.out.println("Puppy's is "+ name );
+	  }
+	public static void main(String[]args) {
+	    // Following statement would create an object myPuppy
+	Puppy myPuppy =new Puppy("tommy");
+	  }
+	}
