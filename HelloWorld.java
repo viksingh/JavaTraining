e04402678042e79afad85d061c6aa89153e42947
viksingh <vikas.singh.0@gmail.com>
2014-02-14 13:58:41 +1100

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+package demo.training;
+//Program 1 
+
+
+public class HelloWorld {
+
+	
+	/**
+	  @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
